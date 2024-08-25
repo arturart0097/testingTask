3236e95aa9a9a286b2e3783f97aee7814d7dff73
Chipon <youmaharadchu@gmail.com>
2024-08-25 16:27:40 +0300

    Додано активну сторінку

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,10 +4,13 @@ import "./style.css";
 export default function Header() {
   return (
     <header>
-      <NavLink to="/edit-user">
+      <NavLink
+        to="/edit-user"
+        className={({ isActive }) => (isActive ? "active" : "")}
+      >
         <button>Edit User</button>
       </NavLink>
-      <NavLink to="/">
+      <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
         <button>Users</button>
       </NavLink>
     </header>
